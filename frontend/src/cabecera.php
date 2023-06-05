e707279ee1a1b018489029a0cb8b959dbed7746c
Benjamín Mena Ardura <102114557+SlowFloww@users.noreply.github.com>
2023-06-05 13:50:35 -0400

    Add files via upload

---
+++ frontend/src/cabecera.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>System Messenger</title>
+    <link rel="stylesheet" href="stylee.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"> 
+  </head>
+  
+  <body id="fondo4">
+
+<div class="container">
+    <h5 style="color:#000000" >System Messenger</h5>
+    <a id="perfil"style="color:#FFF5EE" href="index.php"> Inicio </a> |
+    <a id="perfil" style="color:#FFF5EE" href="login.php"> Ingresar </a> |
+    <a id="perfil" style="color:#FFF5EE" href="index.php"> Cerrar </a>
+    <hr>
+</div>  
