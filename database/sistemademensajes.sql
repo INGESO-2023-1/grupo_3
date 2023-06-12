479641b88546929f76d1801df90eb396991a8f95
danyba20 <danielbarrigavillanueva@iMac-de-Daniel.local>
2023-06-12 00:52:46 -0400

    minor changes db

--- database/sistemademensajes.sql
+++ database/sistemademensajes.sql
@@ -3,14 +3,17 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+--
+-- Table structure for table `usuarios y chat`
+--
 
---
--- Table structure for table `chat`
---
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id_user` int(12) NOT NULL AUTO_INCREMENT,
+  `user` varchar(20) NOT NULL,
+  `number` varchar(12) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `chat` (
   `id_chat` int(12) NOT NULL AUTO_INCREMENT,
@@ -22,49 +25,3 @@ CREATE TABLE IF NOT EXISTS `chat` (
   FOREIGN KEY (`id_emisor`) REFERENCES `usuarios`(`id_user`),
   FOREIGN KEY (`id_receptor`) REFERENCES `usuarios`(`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
---
--- Table structure for table `register`
---
-
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id_user` int(12) NOT NULL,
-  `user` varchar(20) NOT NULL,
-  `number` varchar(12) NOT NULL,
-  `pass` varchar(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `chat`
---
-ALTER TABLE `chat`
-  ADD PRIMARY KEY (`id_user`);
-
---
--- Indexes for table `register`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id_user`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `chat`
---
-ALTER TABLE `chat`
-  MODIFY `id_user` int(12) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `register`
---
-ALTER TABLE `usuarios`
-  MODIFY `id_user` int(12) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
